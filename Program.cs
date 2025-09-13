fa07b7bdf3179d71b723eb6a1d0fec3a587f1910
Marcos <marcospaulomk09@gmail.com>
2025-09-12 22:42:37 -0300

    Exercício Finalizado.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿
+Console.Clear();
+Console.WriteLine("CidadesDoLitoralSul ");
+Console.ForegroundColor = ConsoleColor.Red;
+Console.WriteLine("Mongaguá ");
+Console.ForegroundColor = ConsoleColor.Blue;
+Console.WriteLine("Peruíbe. ");
+Console.ForegroundColor = ConsoleColor.Green;
+Console.WriteLine("Itanhaém. ");
+Console.ForegroundColor = ConsoleColor.DarkMagenta;
+Console.WriteLine("Praia Grande ");
+Console.ResetColor();
